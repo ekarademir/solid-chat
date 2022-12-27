97f606fb52c57a2cd4fbbe5d04dae14c4d39f0f4
Ertugrul Karademir <ekarademir@gmail.com>
2022-12-27 08:55:43 +0000

    Align with admin app

--- chatter-client/vite.config.ts
+++ chatter-client/vite.config.ts
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    devtools({
+      autoname: true,
+    }),
+  ],
   server: {
-    port: 3000,
+    port: 8000,
+    host: "rotatingwave.local",
+    https: {
+      key: "../ssl/key.pem",
+      cert: "../ssl/cert.pem",
+    },
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
