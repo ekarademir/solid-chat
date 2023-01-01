9573c20022668532b9445dc7cb8af35d85eb4b3e
Ertugrul Karademir <ekarademir@gmail.com>
2023-01-01 11:27:21 +0000

    Refactor API

--- chatter-server/src/services/tenants.rs
+++ chatter-server/src/services/tenants.rs
@@ -1,7 +1,7 @@
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status};
 
-use crate::chat::{tenants_server::Tenants, Tenant, TenantRequest, TenantResponse};
+use crate::chat::{tenants_server::Tenants, FindRequest, ListRequest, Tenant, TenantResponse};
 use crate::errors;
 use crate::errors::ErrorExt;
 use crate::models::tenant::NewTenant;
@@ -10,6 +10,11 @@ use crate::models::tenant::Tenant as TenantModel;
 use super::ServiceResult;
 
 pub use crate::chat::tenants_server::TenantsServer;
+
+#[tonic::async_trait]
+pub trait Streaming {
+    async fn stream();
+}
 
 #[derive(Default)]
 pub struct TenantsService {}
@@ -29,47 +34,33 @@ impl Tenants for TenantsService {
         })
     }
 
-    async fn delete(&self, req: Request<TenantRequest>) -> ServiceResult<TenantResponse> {
-        super::connect_and(
-            |conn| match TenantModel::find_by_name(conn, &req.get_ref().name) {
+    async fn delete(&self, req: Request<FindRequest>) -> ServiceResult<TenantResponse> {
+        super::connect_and(|conn| {
+            match TenantModel::find_by_name(conn, &req.get_ref().name.unwrap()) {
                 Ok(tenant) => match tenant.delete(conn) {
                     Ok(()) => Ok(Response::new(TenantResponse { tenant: None })),
                     Err(e) => Err(e.into_status()),
                 },
                 Err(e) => Err(e.into_status()),
-            },
-        )
+            }
+        })
     }
 
     type ListStream = ReceiverStream<Result<Tenant, Status>>;
-    async fn list(&self, req: Request<TenantRequest>) -> ServiceResult<Self::ListStream> {
+    async fn list(&self, req: Request<ListRequest>) -> ServiceResult<Self::ListStream> {
         let (tx, rx) = tokio::sync::mpsc::channel(100);
         tokio::spawn(async move {
             match super::connect_to_pg() {
                 Ok(mut conn) => {
                     let req = req.get_ref();
-                    if req.list_all {
-                        match TenantModel::list(&mut conn) {
-                            Ok(tenants) => {
-                                for tenant in tenants {
-                                    tx.send(Ok(tenant.into())).await.unwrap();
-                                }
+                    match TenantModel::list(&mut conn) {
+                        Ok(tenants) => {
+                            for tenant in tenants {
+                                tx.send(Ok(tenant.into())).await.unwrap();
                             }
-                            Err(e) => tx.send(Err(e.into_status())).await.unwrap(),
-                        };
-                    } else if req.name.len() > 0 {
-                        match TenantModel::find_by_name(&mut conn, &req.name) {
-                            Ok(tenant) => tx.send(Ok(tenant.into())).await.unwrap(),
-                            Err(e) => tx.send(Err(e.into_status())).await.unwrap(),
                         }
-                    } else {
-                        tx.send(Err(errors::into_status(
-                            anyhow::Error::new(errors::ServiceError::EmptyRequestFields)
-                                .context("list_all, name"),
-                        )))
-                        .await
-                        .unwrap();
-                    }
+                        Err(e) => tx.send(Err(e.into_status())).await.unwrap(),
+                    };
                 }
                 Err(e) => tx.send(Err(e.into_status())).await.unwrap(),
             }
