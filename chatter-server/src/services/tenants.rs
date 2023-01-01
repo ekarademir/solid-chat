e99dca589d59f1eee05c028bbb16f83b52466f0f
Ertugrul Karademir <ekarademir@gmail.com>
2023-01-01 12:24:58 +0000

    Better pattern

--- chatter-server/src/services/tenants.rs
+++ chatter-server/src/services/tenants.rs
@@ -1,8 +1,8 @@
+use futures::future::join_all;
 use tokio_stream::wrappers::ReceiverStream;
 use tonic::{Request, Response, Status};
 
 use crate::chat::{tenants_server::Tenants, FindRequest, ListRequest, Tenant, TenantResponse};
-use crate::errors;
 use crate::errors::ErrorExt;
 use crate::models::tenant::NewTenant;
 use crate::models::tenant::Tenant as TenantModel;
@@ -36,13 +36,10 @@ impl Tenants for TenantsService {
 
     async fn delete(&self, req: Request<FindRequest>) -> ServiceResult<TenantResponse> {
         super::connect_and(|conn| {
-            match TenantModel::find_by_name(conn, &req.get_ref().name.unwrap()) {
-                Ok(tenant) => match tenant.delete(conn) {
-                    Ok(()) => Ok(Response::new(TenantResponse { tenant: None })),
-                    Err(e) => Err(e.into_status()),
-                },
-                Err(e) => Err(e.into_status()),
-            }
+            TenantModel::find_by_name(conn, &req.get_ref().name.as_deref().unwrap_or(""))
+                .and_then(|tenant| tenant.delete(conn))
+                .and_then(|_| Ok(Response::new(TenantResponse { tenant: None })))
+                .map_err(|e| e.into_status())
         })
     }
 
@@ -50,20 +47,25 @@ impl Tenants for TenantsService {
     async fn list(&self, req: Request<ListRequest>) -> ServiceResult<Self::ListStream> {
         let (tx, rx) = tokio::sync::mpsc::channel(100);
         tokio::spawn(async move {
-            match super::connect_to_pg() {
-                Ok(mut conn) => {
+            match super::connect_to_pg()
+                .and_then(|conn| {
                     let req = req.get_ref();
-                    match TenantModel::list(&mut conn) {
-                        Ok(tenants) => {
-                            for tenant in tenants {
-                                tx.send(Ok(tenant.into())).await.unwrap();
-                            }
-                        }
-                        Err(e) => tx.send(Err(e.into_status())).await.unwrap(),
-                    };
+                    TenantModel::list(&mut conn)
+                })
+                .and_then(|tenants| {
+                    Ok(join_all(
+                        tenants.iter().map(|tenant| tx.send(Ok((*tenant).into()))),
+                    ))
+                })
+                .map_err(|e| tx.send(Err(e.into_status())))
+            {
+                Ok(x) => {
+                    x.await;
                 }
-                Err(e) => tx.send(Err(e.into_status())).await.unwrap(),
-            }
+                Err(x) => {
+                    x.await.ok();
+                }
+            };
         });
 
         Ok(Response::new(ReceiverStream::new(rx)))
